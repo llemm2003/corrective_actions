55bf1951d280333d26f005ad0772d9800895f9e1
llemm2003 <rommell.sabino@protonmail.com>
2017-10-28 04:07:48 +0800

    Additional Loop after first loop
    To re mediate the abnormal exit of the script due to script has already reached the end of the loop but the process is still not finished, I added a second loop after the first one to monitor the completion of the process.

--- thread_command.sh
+++ thread_command.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-thread_num=3
+thread_num=4
 counter=0
 pids=""
 
 #Get random number, will not be needed in actual script.
 get_ran () {
-output_num=`echo ${RANDOM:0:1}`
+output_num=`echo ${RANDOM:0:2}`
 echo $output_num
 }
 
-for i in 1 2 3 4 5 6 7 8 9 
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
  echo "$i - location in the loop"
  if [ "$counter" -eq "$thread_num" ]; then
@@ -26,7 +26,7 @@ do
   do
    echo "$counter - num of thread right now"
    echo "Should pause here since thread running is equal to the thread_num var"
-   sleep 1
+   sleep 5
    for j in $pids
    do
     running_pid_flag=`ps -ef | grep $j| grep -v grep| wc -l`
@@ -43,4 +43,23 @@ do
   echo "$counter - new value of counter after passing the test"
  done
  fi
+done
+
+echo '############################################'
+echo "The PIDS running after the code : $pids"
+
+#This loop means this is the last set
+while [ ! -z "$pids" ]
+do
+ for k in $pids
+ do 
+  running_pid_flag=`ps -ef | grep $k| grep -v grep| wc -l`
+  ps -ef | grep $k| grep -v grep
+  if [ "$running_pid_flag" -eq "0" ]; then
+   echo "The pid $k is not existing anymore, removing it from pid list"
+   pids=`echo ${pids/$k/}`
+   echo "Old pids gone, here are the new pids: $pids"
+  fi
+ done
+ sleep 5
 done
